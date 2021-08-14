b914e9d769c8165026108eb8fd445a4a390303d4
en-coded <83879346+en-coded@users.noreply.github.com>
2021-08-14 11:31:31 -0400

    Final commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,32 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 export default function Home(props) {
-	return <div className="HomePage">This is the {props.page} page</div>;
+	const [bookmark, setBookmarks] = useState([]); // <==== Bookmark State
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch('/api/bookmarks');
+				const data = await response.json();
+				setBookmarks(data);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, []);
+	return (
+		<div className="HomePage">
+			<ul>
+				{bookmark.map(bookmark => {
+					return (
+						<li key={bookmark._id}>
+							<Link to={`/${bookmark._id}`}>
+								<h3>{bookmark.title}</h3>
+							</Link>
+							<p>{bookmark.link}</p>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
